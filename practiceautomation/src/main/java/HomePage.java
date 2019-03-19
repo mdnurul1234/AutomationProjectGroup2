a3bf2cba6d9bc1241f9b960a0e50906594250803
kingcrownamir64 <amirhafiz08@gmail.com>
2019-03-19 16:47:04 -0400

    Corrections for Framework

--- practiceautomation/src/main/java/HomePage.java
+++ practiceautomation/src/main/java/HomePage.java
@@ -1,2 +1,46 @@
-public class HomePage {
-}
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+    public class HomePage extends BaseClient {
+
+
+        @FindBy(id = "twotabsearchtextbox")
+        private WebElement searchBox;
+
+        @FindBy(className = "nav-input")
+        private WebElement searchButton;
+
+        @FindBy(id = "nav-cart")
+        private WebElement cartButton;
+
+
+        @FindBy(id="nav-your-amazon")
+        private WebElement YourAmazonButton;
+
+        @FindBy(id="icp-touch-link-language")
+        private WebElement LanguageBar;
+
+
+
+        public void searchBoxHome(String text) {
+            this.searchBox.sendKeys(text);
+        }
+
+        public void SearchButtonHome() {
+            this.searchBox.click();
+        }
+
+        public void setCartButton() {
+            this.cartButton.click();
+        }
+
+        public void setYourAmazonButton() {
+            this.YourAmazonButton.click();
+        }
+
+        public void setLanguageBar() {
+            this.LanguageBar.click();
+        }
+
+    }
+
